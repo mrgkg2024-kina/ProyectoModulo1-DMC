72e11f4eb32f4eda9bf924bd2d1a45614421097a
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-07 22:29:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,9 @@ import streamlit as st
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
-valor_inicial =st.numbre_input("Ingrese el valor inicial")
+valor_inicial =st.numbre_input("Ingrese el valor inicial", value =0)
+valor_final =st.numbre_input("Ingrese el valor final", value=1 )
+
+lista_numerica= list(range(valor_inicial, valor_final))
+
+st.write(lista_numerica)

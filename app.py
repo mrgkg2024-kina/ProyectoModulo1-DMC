e5ae2a0d799dc9dfc9ef0e2b7f99a3c3c36c3f6a
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-09 22:15:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
+st.image("Python_logo.png")
+st.sidebar.image("DMC.png")
 
 valor_inicial =st.number_input("Ingrese el valor inicial", value =0)
 valor_final =st.number_input("Ingrese el valor final", value=1 )

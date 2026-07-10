f33d2e642360809f1ab5ef03f045c7cba346870a
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-09 22:30:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
@@ -16,6 +17,16 @@ if modulo == "Módulo Listas":
 
 elif modulo == "Módulo Array":  
    st.write("Estas en el módulo de arreglos")
+   
+   limite_inferior =st.number_input("Ingrese el limite inferior", value =1200)
+   limite_superior =st.number_input("Ingrese el limite superior", value=1250)
+   cantidad_datos =st.number_input("Ingrese totalidad de datos a crear", value=31)
+   
+   datos_produccion = np.random.randInt(limite_inferior,limite_superior, cantidad_datos)
+   st.write(datos_produccion) 
+   st.write("La producción total es:" ,  np.sum(datos_produccion))
+   st.write("La producción promedio es:" , np.mean(datos_produccion) 
+  
 
 else:
    st.write("Estas en el módulo de funciones")
